553a8f7977ee12e28574de13224a1373a210f096
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-30 01:34:14 -0300

    Validaçoes

--- redefir_senha/lib/primeira_tela.dart
+++ redefir_senha/lib/primeira_tela.dart
@@ -5,8 +5,6 @@ import 'package:redefir_senha/validatores.dart';
 import 'package:validatorless/validatorless.dart';
 
 class PrimeiraTela extends StatefulWidget {
- 
-
   PrimeiraTela({super.key});
 
   @override
@@ -25,10 +23,11 @@ class _PrimeiraTelaState extends State<PrimeiraTela> {
     _password.dispose();
     _passwordConfirm.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Form(
-      key : _formKey,
+      key: _formKey,
       child: Scaffold(
         body: SingleChildScrollView(
           child: Container(
@@ -49,12 +48,11 @@ class _PrimeiraTelaState extends State<PrimeiraTela> {
                   padding: const EdgeInsets.only(top: 40, right: 30, left: 30),
                   child: TextFormField(
                     controller: _password,
-                     validator: Validatorless.multiple([
-                        Validatorless.required("Digite sua nova senha"),
-                        Validatorless.min(8, "Senha precisa ter no minimo 8 caracteres"),
-
-                        
-                     ]),
+                    validator: Validatorless.multiple([
+                      Validatorless.required("Digite sua nova senha"),
+                      Validatorless.min(
+                          8, "Senha precisa ter no minimo 8 caracteres"),
+                    ]),
                     decoration: InputDecoration(
                         hintText: "Digite sua nova senha",
                         enabledBorder: const UnderlineInputBorder(
@@ -75,16 +73,16 @@ class _PrimeiraTelaState extends State<PrimeiraTela> {
                     obscureText: _showPassword == false ? true : false,
                   ),
                 ),
-                Padding( 
+                Padding(
                   padding: const EdgeInsets.only(top: 50, right: 30, left: 30),
                   child: TextFormField(
                     controller: _passwordConfirm,
                     validator: Validatorless.multiple([
-                        Validatorless.required("Confirme sua nova senha"),
-                        Validatorless.min(8, "Senha precisa ter no minimo 8 caracteres"),
-                        
-                        Validators.compare(_password, "Digite uma senha valida")
-                     ]),
+                      Validatorless.required("Confirme sua nova senha"),
+                      Validatorless.min(
+                          8, "Senha precisa ter no minimo 8 caracteres"),
+                      Validators.compare(_password, "Digite uma senha valida")
+                    ]),
                     decoration: InputDecoration(
                         hintText: "Confirme sua nova senha",
                         enabledBorder: const UnderlineInputBorder(
@@ -109,22 +107,21 @@ class _PrimeiraTelaState extends State<PrimeiraTela> {
                   padding: const EdgeInsets.only(
                     top: 45,
                   ),
-                  child:  Container(
-                    child: ElevatedButton(onPressed: (){
-                      var formValid = _formKey.currentState?.validate() ?? false;
-                      if(formValid){
-                        
-                      }
-                    }, child: Text("Confirmar"),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24))
-                    ),
+                  child: Container(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        var formValid =
+                            _formKey.currentState?.validate() ?? false;
+                        if (formValid) {}
+                      },
+                      child: Text("Confirmar"),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24))),
                     ),
                     width: 313,
-                    
-                    ),
                   ),
-                
+                ),
               ],
             ),
           ),
